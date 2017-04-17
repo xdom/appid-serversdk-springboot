82206f80cf66ce62eed01c6d24f3c739581dac37
xdom <xdomo@azet.sk>
2017-04-17 20:36:56 +0200

    Initial commit

---
+++ src/main/java/sk/tuke/cloud/security/MicroserviceJwtApplication.java
@@ -0,0 +1,12 @@
+package sk.tuke.cloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceJwtApplication.class, args);
+	}
+}

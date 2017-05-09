19fcd8923b42cff5267c35ee9666a4d1d3bcc81e
xdom <xdomo@azet.sk>
2017-05-09 21:22:02 +0200

    Fix spring.factories

--- src/main/java/com/ibm/mobilefirstplatform/appid/SecurityConfiguration.java
+++ src/main/java/com/ibm/mobilefirstplatform/appid/SecurityConfiguration.java
@@ -2,10 +2,7 @@ package com.ibm.mobilefirstplatform.appid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,8 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
  */
 @Configuration
 @ConditionalOnClass({EnableWebSecurity.class})
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final PublicKeyProvider publicKeyProvider;
 
